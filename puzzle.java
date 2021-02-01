4953798740af95246ee33bd8d7c01b248d07dd34
Lucas Kempe <lucas.kempe@icloud.com>
2021-02-01 11:27:33 -0300

    Puzzle solved...

---
+++ puzzle.java
@@ -0,0 +1,24 @@
+
+import java.math.BigInteger;
+
+class Puzzle {
+
+final static BigInteger M = new BigInteger("2017");
+
+private static BigInteger compute(long n) {
+String s = "";
+String a = new BigInteger(n+"").mod(M)+"";
+for (long i=0; i<Integer.parseInt(a); i++) {
+    s += a;
+    s = new BigInteger(s).mod(M)+"";
+}
+
+return new BigInteger(s.toString()).mod(M);
+
+}
+
+public static void main(String args[]) {
+for (long n : new long[] { 1L, 2L, 5L, 10L, 20L, 58184241583791680L }) {
+System.out.println("" + n + ": " + compute(n));
+}
+}}
